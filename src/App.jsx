8ea217fe7296e261dc8af98cf8c5921bc1788ecd
dev267-web <devendrarathore12005@gmail.com>
2025-08-13 00:42:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ContactForm from './components/ContactForm/ContactForm.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
